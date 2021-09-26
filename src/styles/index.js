c158962613f7aaaaa9a70d1eff2107b1b665d24b
SlenderMenon <nandasid10@gmail.com>
2021-09-27 00:48:10 +0530

    Added comm + Boostrap stylesheet, comps

---
+++ src/styles/index.js
@@ -0,0 +1,13 @@
+// boostrap components
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+// bootstrap styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// common styling
+import './comms.css';
+
+export {
+    Nav, Navbar
+}
