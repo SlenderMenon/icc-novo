a8be9c358629e6a180cb1884bf2ae14c5ae02e00
SlenderMenon <nandasid10@gmail.com>
2021-09-27 12:31:16 +0530

    Added carousel, image picker bar

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -66,6 +66,89 @@ export class Content extends React.Component {
 
             </Col>
 
+            <Col className="center-pane" lg="9">
+
+              <Row className="carousel-container">
+                <Col>
+                  <Carousel className="carousel-element">
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=First slide&bg=373940"
+                        alt="First slide"
+                      />
+                      <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="Second slide"
+                      />
+
+                      <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                      />
+
+                      <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
+                </Col>
+              </Row>
+
+              <Row className="carousel-images-container bg-light">
+                <Col>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                  <div className="carousel-preview-container">
+                    <div className="carousel-preview-image"></div>
+                    <h7>Landscape-5.png</h7>
+                  </div>
+                </Col>
+              </Row>
+
+            </Col>
+
           </Row>
 
         </Container>
