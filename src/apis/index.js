3a5309fe33a377500a19458226b9663b76380623
SlenderMenon <nandasid10@gmail.com>
2021-09-27 21:11:23 +0530

    Upgraded APIs, added Unsplash

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 export default {
 
-  SERVER_URL: 'http://localhost/',
+  SERVER_URL: 'http://localhost:8081',
+  UNSPLASH: {
+    SERVER_URL: 'https://api.unsplash.com',
+    KEY: 'Client-ID _wPDVNUmeDRW1AaUi-SE9CyzWD-imab1x3MOqvzuvcc'
+  },
 
   async getTopics() {
     try {
       const axiosInstance = axios.create({ baseURL: this.SERVER_URL });
-      const res = (await axiosInstance.get('http://localhost:8081/topics')).data;
+      const res = (await axiosInstance.get('/topics')).data;
       return res;
     } catch (err) {
       console.log('Error in API call with local server: ', err);  // @TODO wrap in error object
@@ -19,8 +23,22 @@ export default {
     try {
       const axiosInstance = axios.create({ baseURL: this.SERVER_URL });
       const res = (await axiosInstance.get(
-        'http://localhost:8081/count',
+        '/count',
         { params: { topic } }
+      )).data;
+      return res;
+    } catch (err) {
+      console.log('Error in API call with local server: ', err);  // @TODO wrap in error object
+      return null;
+    }
+  },
+
+  async getPhotos(topic) {
+    try {
+      const axiosInstance = axios.create({ baseURL: this.UNSPLASH.SERVER_URL });
+      const res = (await axiosInstance.get(
+        `/topics/${topic}/photos`,
+        { headers: { Authorization: this.UNSPLASH.KEY } }
       )).data;
       return res;
     } catch (err) {
