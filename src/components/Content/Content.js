079a73aba8a7f390688dbd551c4d165b81cea8f8
SlenderMenon <nandasid10@gmail.com>
2021-09-27 15:14:00 +0530

    Added draggable

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,13 +8,112 @@ import './Content.css';
 
 // components
 
+// variables
+const DRAGGABLE_TYPES = {
+  LEFT_PANE: 'left-pane-draggable',
+  RIGHT_PANE: 'right-pane-draggable',
+}
+
 export class Content extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      dropdownSelected: 'Select Topic'
+      dropdownSelected: 'Select Topic',
+      fileList: [
+        'Landscape-1.png',
+        'Landscape-2.png',
+        'Landscape-3.png',
+        'Landscape-4.png',
+        'Landscape-5.png',
+        'Landscape-6.png',
+        'Landscape-7.png',
+        'Landscape-8.png',
+        'Landscape-9.png',
+        'Landscape-10.png'
+      ],
+      carouselPreviewImages: [],
     }
+
+    window.addEventListener('DOMContentLoaded', () => {
+      this.addDraggableListenerToFileList();
+    });
+  }
+
+  dragOver = (ev) => {
+    ev.preventDefault();
+  }
+
+  dropOverFilePicker = (ev) => {
+    ev.preventDefault();
+    console.log(ev.dataTransfer.getData("application/json"));
+    const data = JSON.parse(ev.dataTransfer.getData("application/json"));
+    if (data.source !== DRAGGABLE_TYPES.RIGHT_PANE) {
+      console.log('Cannot drop here!');  // @TODO set error status
+      return;
+    }
+    const { fileList } = this.state;
+    fileList.push(data.title);
+    this.setState({ fileList });
+    console.log('STATE UPDATED!', this.state.fileList);
+  }
+
+  dropOverCarouselPicker = (ev) => {
+    ev.preventDefault();
+    console.log(ev.dataTransfer.getData("application/json"));
+    const data = JSON.parse(ev.dataTransfer.getData("application/json"));
+    if (data.source !== DRAGGABLE_TYPES.LEFT_PANE) {
+      console.log('Cannot drop here!');  // @TODO set error status
+      return;
+    }
+    const { carouselPreviewImages } = this.state;
+    carouselPreviewImages.push({
+      title: data.title
+    });
+    this.setState({ carouselPreviewImages });
+    console.log('STATE UPDATED!', this.state.carouselPreviewImages);
+  }
+
+  addDraggableListenerToFileList() {
+    // add draggable to the file-list
+    Array.from(document.getElementsByClassName('list-group-item')).forEach((listGroupItem) => {
+      listGroupItem.addEventListener("dragstart", (ev) => {
+        ev.dataTransfer.setData("application/json", JSON.stringify({
+          title: ev.target.innerText,
+          source: DRAGGABLE_TYPES.LEFT_PANE
+        }));
+        console.log(ev.target.innerText);
+      });
+    });
+  }
+
+  addDraggableListenerToCarouselPicker() {
+    // add draggable to the carousel-picker
+    Array.from(document.getElementsByClassName('carousel-preview-container')).forEach((carouselPreviewItem) => {
+      carouselPreviewItem.addEventListener("dragstart", (ev) => {
+        console.log(ev);
+        ev.dataTransfer.setData("application/json", JSON.stringify({
+          title: ev.srcElement.children[1].innerText,
+          source: DRAGGABLE_TYPES.RIGHT_PANE
+        }));
+      });
+    });
+  }
+
+  getCarouselImagePreviewItem(imageName) {
+    return (
+      <div className="carousel-preview-container" draggable="true">
+        <div className="carousel-preview-image"></div>
+        <h7 className="carousel-preview-image-title">{imageName}</h7>
+      </div>
+    )
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { prevFileList, prevCarouselPreviewImages } = prevState;
+    const { fileList, carouselPreviewImages } = this.state;    
+    if (!prevFileList && fileList || fileList.length !== prevFileList.length) this.addDraggableListenerToFileList();
+    if (!prevCarouselPreviewImages && carouselPreviewImages || prevCarouselPreviewImages.length !== carouselPreviewImages.length) this.addDraggableListenerToCarouselPicker();
   }
 
   render() {
@@ -43,24 +142,12 @@ export class Content extends React.Component {
 
               <div className="file-list-container">
                 <h6>Files</h6>
-                <ListGroup>
-                  <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                  <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                  <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup onDragOver={this.dragOver} onDrop={this.dropOverFilePicker}>
+                  {
+                    this.state.fileList.map((fileName) =>
+                      <ListGroup.Item draggable="true">{fileName}</ListGroup.Item>
+                    )
+                  }
                 </ListGroup>
               </div>
 
@@ -111,39 +198,12 @@ export class Content extends React.Component {
               </Row>
 
               <Row className="carousel-images-container bg-light">
-                <Col>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
-                  <div className="carousel-preview-container">
-                    <div className="carousel-preview-image"></div>
-                    <h7>Landscape-5.png</h7>
-                  </div>
+                <Col id="coursel-images-drop-container" onDragOver={this.dragOver} onDrop={this.dropOverCarouselPicker}>
+                  {
+                    this.state.carouselPreviewImages.map((carouselPreviewImage) =>
+                      this.getCarouselImagePreviewItem(carouselPreviewImage.title)
+                    )
+                  }
                 </Col>
               </Row>
 
