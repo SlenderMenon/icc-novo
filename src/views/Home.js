c87b0cb8e76c797acad7e6073481a93606bc041e
SlenderMenon <nandasid10@gmail.com>
2021-09-28 14:10:45 +0530

    Updated root component

---
+++ src/views/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+// bootstrap
+
+// styling
+import './Home.css';
+
+// components
+import Titlebar from '../components/Titlebar/Titlebar';
+import Content from '../components/Content/Content';
+import Message from '../components/Message/Message';
+
+export class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: {
+        type: 'neutral',
+        info: 'Idle'
+      },
+      carouselConfig: [],
+      forceReRender: 0,
+    }
+  }
+
+  getCarouselConfig = () => {
+    return this.state.carouselConfig;
+  }
+  setCarouselConfig = (carouselConfig, mustForceReRender) => {
+    this.setState({
+      carouselConfig,
+      forceReRender: mustForceReRender ? Math.random() : this.state.forceReRender
+    });
+  }
+
+  setMessage = (type, info) => {
+    this.setState({
+      message: { type, info }
+    });
+  }
+
+  render() {
+    return (
+      <div className="home-component">
+        <Titlebar setMessage={this.setMessage} setCarouselConfig={this.setCarouselConfig} />
+        <Content setMessage={this.setMessage} carouselConfig={this.state.carouselConfig} setCarouselConfig={this.setCarouselConfig} forceReRender={this.state.forceReRender} />
+        <Message message={this.state.message} />
+      </div>
+    )
+  }
+}
+
+export default Home;
